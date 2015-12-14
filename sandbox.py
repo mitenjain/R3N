6cdd06fd8996b4ad4bc28e62435c8588cbf88b0b
ArtRand <arand@soe.ucsc.edu>
2015-12-14 09:12:10 -0800

    3-class aNN in buildmodel2

--- sandbox.py
+++ sandbox.py
@@ -36,6 +36,9 @@ def generate_3_class_spiral_data(plot=False):
 
     if plot is True:
         plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+        plt.xlim([-1, 1])
+        plt.ylim([-1, 1])
+
         plt.show()
 
     return X, y
@@ -54,6 +57,23 @@ def plot_decision_boundary(pred_func, X, labels):
     # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.scatter(X[:, 0], X[:, 1], c=labels, cmap=plt.cm.Spectral)
+    plt.show()
+
+
+def plot_decision_boundry2():
+    h = 0.02
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = np.dot(np.maximum(0, np.dot(np.c_[xx.ravel(), yy.ravel()], W) + b), W2) + b2
+    Z = np.argmax(Z, axis=1)
+    Z = Z.reshape(xx.shape)
+    fig = plt.figure()
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
     plt.show()
 
 
@@ -96,7 +116,7 @@ def predict(model, X):
     return np.argmax(probs, axis=1)
 
 
-def build_model(train_data, labels, nn_hdim, reg_lambda=0.01, epsilon=0.01, num_passes=10000, print_loss=False):
+def build_model1(train_data, labels, nn_hdim, reg_lambda=0.01, epsilon=0.01, num_passes=10000, print_loss=False):
     # input dimensions is the size of the rows
     nn_input_dim = len(train_data[0, :])
     nn_output_dim = len(train_data[0, :])
@@ -173,7 +193,105 @@ def build_model(train_data, labels, nn_hdim, reg_lambda=0.01, epsilon=0.01, num_
     return model
 
 
+def build_model2(input_data, labels, nb_classes, nn_hidden_dim):
+    # initialize parameters randomly
+    X = input_data
+    h = nn_hidden_dim  # size of hidden layer
+    D = input_data.shape[1]
+    W = 0.01 * np.random.randn(D, h)
+    b = np.zeros((1, h))
 
-X, y = generate_2_class_moon_data()
-model = build_model(X, y, 3, print_loss=True)
-plot_decision_boundary(lambda x: predict(model, x), X, y)
+    K = nb_classes  # similar to nn_output_dim
+    W2 = 0.01 * np.random.randn(h, K)
+    b2 = np.zeros((1, K))
+
+    # some hyperparameters
+    step_size = 1e-0
+    reg = 1e-3  # regularization strength
+
+    # gradient descent loop
+    num_examples = X.shape[0]
+
+    model = {}
+
+    for i in xrange(10000):
+        # evaluate class scores, [N x K]
+        # this could be forward prop
+        hidden_layer = np.maximum(0, np.dot(X, W) + b) # note, ReLU activation
+        scores = np.dot(hidden_layer, W2) + b2
+
+        # compute the class probabilities
+        exp_scores = np.exp(scores)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # [N x K]
+
+        # compute the loss: average cross-entropy loss and regularization
+        # make this it's own function
+        corect_logprobs = -np.log(probs[range(num_examples), labels])
+        data_loss = np.sum(corect_logprobs) / num_examples
+        reg_loss = 0.5 * reg * np.sum(W * W) + 0.5 * reg * np.sum(W2 * W2)
+        loss = data_loss + reg_loss
+        if i % 1000 == 0:
+            print "iteration %d: loss %f" % (i, loss)
+
+        # compute the gradient on scores
+        dscores = probs
+        dscores[range(num_examples), y] -= 1
+        dscores /= num_examples
+
+        # backpropate the gradient to the parameters
+        # first backprop into parameters W2 and b2
+        dW2 = np.dot(hidden_layer.T, dscores)
+        db2 = np.sum(dscores, axis=0, keepdims=True)
+
+        # next backprop into hidden layer
+        dhidden = np.dot(dscores, W2.T)
+
+        # backprop the ReLU non-linearity
+        dhidden[hidden_layer <= 0] = 0
+
+        # finally into W,b
+        dW = np.dot(X.T, dhidden)
+        db = np.sum(dhidden, axis=0, keepdims=True)
+
+        # add regularization gradient contribution
+        dW2 += reg * W2
+        dW += reg * W
+
+        # perform a parameter update
+        W += -step_size * dW
+        b += -step_size * db
+        W2 += -step_size * dW2
+        b2 += -step_size * db2
+
+        model = {'W1': W, 'b1': b, 'W2': W2, 'b2': b2}
+
+    hidden_layer = np.maximum(0, np.dot(X, W) + b)
+    scores = np.dot(hidden_layer, W2) + b2
+    precicted_class = np.argmax(scores, axis=1)
+    print "training accuracy: %0.2f" % (np.mean(precicted_class == labels))
+
+
+    h = 0.02
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = np.dot(np.maximum(0, np.dot(np.c_[xx.ravel(), yy.ravel()], W) + b), W2) + b2
+    Z = np.argmax(Z, axis=1)
+    Z = Z.reshape(xx.shape)
+    fig = plt.figure()
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.show()
+
+    return model
+
+
+X, y = generate_3_class_spiral_data(plot=False)
+m = build_model2(X, y, 3, 50)
+
+
+
+

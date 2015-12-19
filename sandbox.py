8c2744ad06c82aca349f3595b98828a39a98de99
ArtRand <arand@soe.ucsc.edu>
2015-12-19 11:27:51 -0800

    feature centering changed to between all features and posteriors

--- sandbox.py
+++ sandbox.py
@@ -1,123 +1,14 @@
 #!/usr/bin/env python
 
-import tensorflow as tf
 from toy_datasets import *
 from neural_network import *
 from utils import *
 from activation_functions import *
-import input_data
-
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
-
-
-# get data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
-# start the session
-sess = tf.InteractiveSession()
-
-# placeholder for data and targets
-x = tf.placeholder("float", shape=[None, 784])
-y_ = tf.placeholder("float", shape=[None, 10])
-
-## softmax classifier ##
-'''
-# params
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
-
-# init variables
-sess.run(tf.initialize_all_variables())
-
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-'''
-
-## conv net ##
-
-# reshape x
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-
-# make conv layer 1
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-
-# layer 2
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-keep_prob = tf.placeholder("float")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
-# Evaluate
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-sess.run(tf.initialize_all_variables())
-
-for i in xrange(2000):
-    batch = mnist.train.next_batch(50)
-    if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict = {
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print "step {0}, training accuracy {1}".format(i, train_accuracy)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-
-print "test accuracy {}".format(accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 
 
 
 
-#train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-
-#batch = mnist.train.next_batch(50)
-
-#for i in xrange(1000):
-#    batch = mnist.train.next_batch(50)
-#    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-#correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-
-#accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 
 # generating test data
@@ -164,4 +55,12 @@ print "test accuracy {}".format(accuracy.eval(feed_dict={
 #print net.predict_old(X2)[1:10] == net.predict(X2)[1:10]
 #plot_decision_boundary(lambda x: np.argmax(net.predict(x), axis=1),
 #                       X, Y)
+
+labels = []
+c = "../marginAlign/cPecan/tests/test_alignments/c100/tempFiles_alignment/"
+train, labels, test = collect_data_vectors(c, True, labels, 0, 0.4, 747, 100)
+train1, labels, test1 = collect_data_vectors(c, True, labels, 0, 0.4, 747, 100)
+t = np.vstack((train, train1, test1))
+print np.nan_to_num(t)
+#center_features_on_training_data(train, test)
 
--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def get_motif_range(ref_start, forward, reference_length=891):
         return complement_motif_range
 
 
-def cull_motif_features(start, tsv, forward, zero_center=True):
+def cull_motif_features(start, tsv, forward):
     # load the tsv
     data = np.loadtxt(tsv, dtype=str)
     motif_range = get_motif_range(start, forward)
@@ -24,8 +24,11 @@ def cull_motif_features(start, tsv, forward, zero_center=True):
     # build a feature vector that has the first 6 elements as the template features and the second
     # six elements as the complement features, the features are selected as the ones with the maximum
     # posterior probability
-    feature_vector = np.zeros(12)
-    feature_posteriors = np.zeros(6)  # to keep track of maximum
+    #feature_vector = np.zeros(12)
+    feature_vector = np.empty(12)
+    feature_vector.fill(np.nan)
+
+    feature_posteriors = np.zeros([1, 6])  # to keep track of maximum
 
     for line in data:
         if line[4] == "t" and int(line[0]) in motif_range and forward is True:
@@ -36,13 +39,13 @@ def cull_motif_features(start, tsv, forward, zero_center=True):
             vector_index = e_index * 2
             delta_mean = float(line[5]) - float(line[9])
             # delta_noise = float(line[6]) - float(line[10])  # change in noise not used yet
-            posterior = line[8]
+            posterior = float(line[8])
 
             # if the posterior for this event is higher than the one we have previously seen,
-            if posterior > feature_posteriors[e_index]:
+            if posterior > feature_posteriors[0, e_index]:
                 feature_vector[vector_index] = delta_mean
                 feature_vector[vector_index + 1] = posterior
-                feature_posteriors[e_index] = posterior
+                feature_posteriors[0, e_index] = posterior
         if line[4] == "c" and int(line[0]) in motif_range and forward is False:
             e_index = motif_range.index(int(line[0]))
             vector_index = e_index * 2
@@ -55,16 +58,17 @@ def cull_motif_features(start, tsv, forward, zero_center=True):
                 feature_vector[vector_index + 1] = posterior
                 feature_posteriors[e_index] = posterior
 
-    if zero_center is True:
-        feature_mean = np.mean([feature_vector[i] for i in xrange(0, len(feature_vector), 2) if feature_vector[i] != 0])
-        for i in xrange(0, len(feature_vector), 2):
-            if feature_vector[i] != 0.0:
-                feature_vector[i] -= feature_mean
-
+    #if zero_center is True:
+    #    feature_mean = np.mean([feature_vector[i] for i in xrange(0, len(feature_vector), 2)
+    #                            if feature_vector[i] != 0])
+    #    for i in xrange(0, len(feature_vector), 2):
+    #        if feature_vector[i] != 0.0:
+    #            feature_vector[i] -= feature_mean
+    #return np.nan_to_num(feature_vector)
     return feature_vector
 
 
-def collect_data_vectors(path, forward, labels, label, portion, motif_start, max_samples, center_data):
+def collect_data_vectors(path, forward, labels, label, portion, motif_start, max_samples):
     """collects the training data
     """
     # collect the files
@@ -88,12 +92,12 @@ def collect_data_vectors(path, forward, labels, label, portion, motif_start, max
     test_data = np.zeros([len(tsvs) - split_index, 12])
 
     for i, f in enumerate(tsvs[:split_index]):
-        vector = cull_motif_features(motif_start, path + f, forward, center_data)
+        vector = cull_motif_features(motif_start, path + f, forward)
         train_data[i:i + 1] = vector
         labels.append(label)  # TODO move this out of the function
 
     for i, f in enumerate(tsvs[split_index:]):
-        vector = cull_motif_features(motif_start, path + f, forward, center_data)
+        vector = cull_motif_features(motif_start, path + f, forward)
         test_data[i:i+1] = vector
 
     return train_data, labels, test_data
@@ -107,6 +111,15 @@ def shuffle_and_maintain_labels(data, labels):
     y = [x[1] for x in dataset]
 
     return np.asarray(X), y
+
+
+def center_features_on_training_data(training_vectors, test_vectors):
+    feature_mean_vector = np.nanmean(training_vectors, axis=0)
+    #centered_training = training_vectors - feature_mean_vector
+    #centered_test = test_vectors - feature_mean_vector
+    training_vectors -= feature_mean_vector
+    test_vectors -= feature_mean_vector
+    return training_vectors, test_vectors
 
 
 def classify_with_network(c_alignments, mc_alignments, hmc_alignments,
@@ -128,17 +141,23 @@ def classify_with_network(c_alignments, mc_alignments, hmc_alignments,
         labels = []
         c_train, labels, c_test = collect_data_vectors(c_alignments, forward, labels, 0,
                                                        train_test_split, motif_start_position,
-                                                       max_samples, center_data)
+                                                       max_samples)
         mc_train, labels, mc_test = collect_data_vectors(mc_alignments, forward, labels, 1,
                                                          train_test_split, motif_start_position,
-                                                         max_samples, center_data)
+                                                         max_samples)
         hmc_train, labels, hmc_test = collect_data_vectors(hmc_alignments, forward, labels, 2,
                                                            train_test_split, motif_start_position,
-                                                           max_samples, center_data)
-
+                                                           max_samples)
         training_data = np.vstack((c_train, mc_train, hmc_train))
 
-        X, y = shuffle_and_maintain_labels(training_data, labels)
+        ## added feature centering  ## TODO make optional
+        feature_mean = np.nanmean(training_data, axis=0)
+
+        centered_training_data = training_data - feature_mean
+
+        centered_training_data = np.nan_to_num(centered_training_data)
+
+        X, y = shuffle_and_maintain_labels(centered_training_data, labels)
 
         net = NeuralNetwork(input_dim=X.shape[1],
                             nb_classes=len(set(y)),
@@ -171,6 +190,9 @@ def classify_with_network(c_alignments, mc_alignments, hmc_alignments,
         hmc_targets.fill(2)
 
         all_test_data = np.vstack((c_test, mc_test, hmc_test))
+        all_test_data -= feature_mean
+        all_test_data = np.nan_to_num(all_test_data)
+
         all_targets = np.concatenate((c_targets, mc_targets, hmc_targets))
 
         accuracy = net.evaluate(all_test_data, all_targets)
